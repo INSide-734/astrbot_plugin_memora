6a90376258abe6d3a3b9b17d108837a08f5a9a5c
root <root@LumenStream.localdomain>
2026-08-09 20:52:19 +0800

    feat(knowledge): define application ports

--- core/features/knowledge/__init__.py
+++ core/features/knowledge/__init__.py
@@ -1,11 +1,19 @@
 """知识 feature 的公开领域边界。"""
 
+from .contracts import (
+    KnowledgeExtractorPort,
+    KnowledgeSourceReaderPort,
+    KnowledgeStorePort,
+)
 from .domain import KnowledgeEntry, KnowledgeType
 from .infrastructure import KNOWLEDGE_SORT_COLUMNS, KnowledgeStore
 
 __all__ = [
     "KNOWLEDGE_SORT_COLUMNS",
+    "KnowledgeExtractorPort",
     "KnowledgeEntry",
+    "KnowledgeSourceReaderPort",
     "KnowledgeStore",
+    "KnowledgeStorePort",
     "KnowledgeType",
 ]
